f0ecec65701dfc0d0cc4b1ff7c0beaf7568fb99b
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-03 11:34:57 -0700

    Added a message from Jordi

--- src/mercau/Sample.java
+++ src/mercau/Sample.java
@@ -3,5 +3,6 @@ package mercau;
 public class Sample {
     public static void main(String[] args){
         System.out.println("Hello, my name is Carol Mercau.");
+        System.out.println("Hello Carol, my name is Jordi!");
     }
 }
